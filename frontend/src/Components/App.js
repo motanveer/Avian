02b5f787a92bd5c5dfc9e97be87fdc8d39ee2bc9
Mo <mo@motanveer>
2021-01-18 18:12:13 -0500

    Initialzied some stuff, my brain forgot most of this stuff

---
+++ frontend/src/Components/App.js
@@ -0,0 +1,16 @@
+import styles from './App.module.css'
+
+
+import React, { Component } from 'react'
+
+export class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1 className={styles.Shit}>Shit</h1>
+            </div>
+        )
+    }
+}
+
+export default App
