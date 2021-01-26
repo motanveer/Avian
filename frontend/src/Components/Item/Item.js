b458e8e03b6994c7cad71868f961ad2fb258e1a0
Mo <mo@motanveer>
2021-01-26 00:46:07 -0500

    Added selection UI elements

--- frontend/src/Components/Item/Item.js
+++ frontend/src/Components/Item/Item.js
@@ -3,14 +3,27 @@ import styles from './Item.module.css'
 
 export class Item extends Component {
   
+    constructor(props)
+    state={
+        class: styles.Container
+    }
 
     render() {
         return (
-            <button className={styles.Container}>
+            <button onClick={this.handleClick} className={this.state.class}>
                 <p className={styles.Resource_Title}>{this.props.title}</p>
                 <p className={styles.Resource_Size}>{this.props.size}</p>
             </button>
         )
+    }
+
+    handleClick = (e) =>{
+        if (this.state.class === styles.Container){
+            this.setState({class: styles.Container_Active})
+        } 
+        else{
+            this.setState({class: styles.Container})
+        }
     }
 
 }
