15b8bc2ee80af8eb616640f1b38cd60bacafec60
Mo <mo@motanveer>
2021-01-27 00:10:15 -0500

    Made progress area collapsable and expandable

--- frontend/src/Components/Progress/Progress.js
+++ frontend/src/Components/Progress/Progress.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react'
 import styles from './Progress.module.css'
+import openEye from './visible.svg'
+import closedEye from './nonVisible.svg'
 
 export class Progress extends Component {
+    
+    state ={
+        display: {visible: false, className: styles.Container_Hidden, icon: openEye}
+    }
+
     render() {
         return (
-            <div className={styles.Container_Hidden}>
-                <p>s</p>
+            <div className={this.state.display.className}>
+                <img className={styles.icon} onClick={this.handleDisplay} width="50px" src={this.state.display.icon}/>
             </div>
         )
     }
+
+    handleDisplay = () =>{
+       if(this.state.display.visible){
+       this.setState({display: {visible: false, className: styles.Container_Hidden, icon: openEye}})
+       }
+       else{
+        this.setState({display: {visible: true, className: styles.Container_Active, icon: closedEye}})
+       }
+    }
+
 }
 
 export default Progress
